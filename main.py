0f583026716c453d25e4cdbb73e4051ce52d8551
John-Leiweihao <138362179+John-Leiweihao@users.noreply.github.com>
2024-03-13 10:51:13 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,45 @@
+import streamlit as st
+from llama_index.core import VectorStoreIndex, ServiceContext, Document, SimpleDirectoryReader
+import os
+import openai
+from llama_index.llms.openai import OpenAI
+from llama_index.core.memory import ChatMemoryBuffer
+from llama_index.core.llms import ChatMessage, MessageRole
+openai.api_key = st.secrets["OPENAI_API_KEY"]
+api_base = "https://pro.aiskt.com/v1"
+openai.base_url = api_base
+st.set_page_config(page_title="Chat with the Power electronic robot", page_icon="💎", layout="centered",
+                   initial_sidebar_state="auto", menu_items=None)
+st.title("Chat with the Power electronic robot🤖, powered by LlamaIndex 🙂")
+st.info( "Hello, I am a robot designed specifically for converters!", icon="🤟")
+
+@st.cache_resource(show_spinner=False)
+def load_data():
+    with st.spinner(text="Loading and indexing the buck-boost docs – hang tight! This should take 1-2 minutes."):
+        docs = SimpleDirectoryReader("Modulation评价体系.docx").load_data()
+        service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-4-0125-preview", temperature=0.1,system_prompt="You are an expert in power supply design in the field of power electronics, and your job is to answer technical questions.Assume that all problems are related to the power supply design.Keep your answers technical and fact-based -- don't hallucinate."))
+        index = VectorStoreIndex.from_documents(docs, service_context=service_context)
+        return index
+
+
+index = load_data()
+chat_engine = index.as_chat_engine( chat_mode="context")
+
+for message in st.session_state.messages:  # Display the prior chat messages
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
+if prompt := st.chat_input("Your question"):  # Prompt for user input and save to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    messages_history = [
+    ChatMessage(role=MessageRole.USER if m["role"] == "user" else MessageRole.ASSISTANT, content=m["content"])
+    for m in st.session_state.messages
+]
+    with st.chat_message("assistant"):
+        with st.spinner("Thinking..."):
+            response = chat_engine.chat(prompt, messages_history)
+            st.write(response.response)
+            message = {"role": "assistant", "content": response.response}
+            st.session_state.messages.append(message)
