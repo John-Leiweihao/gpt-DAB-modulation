8fa27eed7625b967966f44f66599014e23e91f7f
John-Leiweihao <138362179+John-Leiweihao@users.noreply.github.com>
2024-03-15 17:12:04 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ from llama_index.core import VectorStoreIndex, ServiceContext, Document, SimpleD
 import os
 import openai
 from llama_index.llms.openai import OpenAI
-from llama_index.core.memory import ChatMemoryBuffer
 from llama_index.core.llms import ChatMessage, MessageRole
+import final_score
+import ast
 openai.api_key = st.secrets["OPENAI_API_KEY"]
 api_base = "https://pro.aiskt.com/v1"
 openai.base_url = api_base
@@ -12,13 +13,19 @@ st.set_page_config(page_title="Chat with the Power electronic robot", page_icon=
                    initial_sidebar_state="auto", menu_items=None)
 st.title("Chat with the Power electronic robot🤖, powered by LlamaIndex 🙂")
 st.info( "Hello, I am a robot designed specifically for converters!", icon="🤟")
-if "messages" not in st.session_state:  # Initialize the chat messages history
-    st.session_state.messages = [ ]
+
+clear_button=st.sidebar.button('Clear Conversation',key='clear')
+with open('./prompt.txt', 'r') as file:
+    content1 = file.read()
+if clear_button or "messages" not in st.session_state:  # Initialize the chat messages history
+    st.session_state.messages = [{"role": "user", "content": content1},
+                                 {"role": "assistant", "content": "OK,I understand."}
+                                 ]
 @st.cache_resource(show_spinner=False)
 def load_data():
     with st.spinner(text="Loading and indexing the buck-boost docs – hang tight! This should take 1-2 minutes."):
         docs = SimpleDirectoryReader("data").load_data()
-        service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-4-0125-preview", temperature=0.1))
+        service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-4-0125-preview", temperature=0.1,system_prompt="You are now an expert in the power electronics industry, and you are proficient in various modulation methods of dual active bridge. Keep your answers technical and fact-based -- don't hallucinate."))
         index = VectorStoreIndex.from_documents(docs, service_context=service_context)
         return index
 
@@ -40,6 +47,16 @@ if prompt := st.chat_input("Your question"):  # Prompt for user input and save t
 ]
     with st.chat_message("assistant"):
         with st.spinner("Thinking..."):
+            response = chat_engine.chat(prompt, messages_history)
+            answer_list = ast.literal_eval(response.response)
+            best_modulation=final_score.recommend_modulation(answer_list)
+            response1="According to your requirements, I recommend you to use the {} modulation strategy".format(best_modulation)
+            st.session_state.messages.append({"role": "assistant", "content": response1})
+            messages_history = [
+                ChatMessage(role=MessageRole.USER if m["role"] == "user" else MessageRole.ASSISTANT,
+                            content=m["content"])
+                for m in st.session_state.messages
+            ]
             response = chat_engine.chat(prompt, messages_history)
             st.write(response.response)
             message = {"role": "assistant", "content": response.response}
