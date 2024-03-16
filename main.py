78064887fd76857b624b8b18d20d4ce3ac19da54
John-Leiweihao <138362179+John-Leiweihao@users.noreply.github.com>
2024-03-16 16:45:45 +0800

    Update main.py

--- main.py
+++ main.py
@@ -33,6 +33,7 @@ def load_data():
 
 index = load_data()
 chat_engine = index.as_chat_engine( chat_mode="context")
+global M
 M="X"
 for message in st.session_state.messages[2:]:  # Display the prior chat messages
     with st.chat_message(message["role"]):
