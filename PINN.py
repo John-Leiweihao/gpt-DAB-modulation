ee914ac1ec3035a83a1fde91909ebcf7d2faa278
John-Leiweihao <138362179+John-Leiweihao@users.noreply.github.com>
2024-03-16 15:55:15 +0800

    Add files via upload

---
+++ PINN.py
@@ -0,0 +1,15 @@
+def PINN(Uin,Uo,Prated,fsw,modulation):
+    if modulation=="SPS":
+        if Uin>0 and Uo>0 and Prated>0 and fsw>0:
+            D0=0.5
+            current_stress=1
+            efficiency=0.9
+        else:
+            D0=1
+            current_stress = 2
+            efficiency=0.8
+    return D0,current_stress,efficiency
+modulation="SPS"
+D0,current_stress,efficiency=PINN(900,800,100e3,50e3,modulation)
+print(D0,current_stress,efficiency)
+
