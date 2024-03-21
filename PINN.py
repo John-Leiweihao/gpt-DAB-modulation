5cda338e709304a531200b41bf0a1fb82fa9dbca
John-Leiweihao <138362179+John-Leiweihao@users.noreply.github.com>
2024-03-21 10:05:52 +0800

    Update PINN.py

--- PINN.py
+++ PINN.py
@@ -3,13 +3,24 @@ def pinn(Uin,Uo,Prated,fsw,modulation):
     current_stress = None
     efficiency = None
     if modulation=="SPS":
-        if Uin>0 and Uo>0 and Prated>0 and fsw>0:
-            D0=0.5
-            current_stress=1
-            efficiency=0.9
-        else:
-            D0=1
-            current_stress = 2
-            efficiency=0.8
+        D0=0.5
+        current_stress=1
+        efficiency=0.9
+    elif modulation=="EPS":
+        D0=1
+        current_stress = 2
+        efficiency=0.8
+    elif modulation=="DPS":
+        D0=1
+        current_stress = 2
+        efficiency=0.8
+    elif modulation=="TPS":
+        D0=1
+        current_stress = 2
+        efficiency=0.8
+    elif modulation=="Five-Degree":
+        D0=1
+        current_stress = 2
+        efficiency=0.8
     return D0,current_stress,efficiency
 
