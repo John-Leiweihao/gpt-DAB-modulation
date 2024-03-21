23b7423a19ca4d5c93f33e07f27d9acf4d665549
John-Leiweihao <138362179+John-Leiweihao@users.noreply.github.com>
2024-03-21 11:16:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -57,8 +57,8 @@ chat_engine = index.as_chat_engine( chat_mode="context")
 for message in st.session_state.messages:  # Display the prior chat messages
     with st.chat_message(message["role"]):
         st.write(message["content"])
-        if "image" in message:
-          st.image(message["image"])
+        if "images" in message:
+          st.image(message["images"])
 
 if prompt := st.chat_input("Your question"):  # Prompt for user input and save to chat history
     st.session_state.messages.append({"role": "user", "content": prompt})
