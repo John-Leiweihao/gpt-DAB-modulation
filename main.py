8a834630ef795a76e6ae68d2533c745dfd0fc5d8
John-Leiweihao <138362179+John-Leiweihao@users.noreply.github.com>
2024-03-28 09:35:58 +0800

    Update main.py

--- main.py
+++ main.py
@@ -73,7 +73,7 @@ index0 = load_data0()
 chat_engine = index0.as_chat_engine( chat_mode="context")
 index1 = load_data1()  
 chat_engine1 = index1.as_chat_engine( chat_mode="context")
-for message in st.session_state.messages:  # Display the prior chat messages
+for message in st.session_state.messages[2:]:  # Display the prior chat messages
     with st.chat_message(message["role"]):
         st.write(message["content"])
         if "images" in message:
