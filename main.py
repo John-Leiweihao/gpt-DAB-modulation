da247d51e64bed7ae5749f2987be727fe3b2ba4d
John-Leiweihao <138362179+John-Leiweihao@users.noreply.github.com>
2024-04-02 11:51:47 +0800

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ import matplotlib.pyplot as plt
 openai.api_key = st.secrets["OPENAI_API_KEY"]
 api_base = "https://pro.aiskt.com/v1"
 openai.base_url = api_base
-st.set_page_config(page_title="Chat with the Power electronic robot", page_icon="💎", layout="centered",
+st.set_page_config(page_title="PE-GPT", page_icon="💎", layout="centered",
                    initial_sidebar_state="auto", menu_items=None)
 st.title("Chat with the Power electronic robot🤖, powered by LlamaIndex 🙂")
 st.info( "Hello, I am a robot designed specifically for converters!", icon="🤟")
