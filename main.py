e0b63b522d0e9870cbf5c44f3ee66476d23ea25d
John-Leiweihao <138362179+John-Leiweihao@users.noreply.github.com>
2024-04-06 17:03:19 +0800

    Update main.py

--- main.py
+++ main.py
@@ -115,7 +115,6 @@ if prompt := st.chat_input("Your question"):  # Prompt for user input and save t
                     st.session_state.M = first_method_found
               message = {"role": "assistant", "content": response.response}
               st.session_state.messages.append(message)
-              st.session_state.messages.append(message)
     elif any(keyword in prompt for keyword in ["PE-GPT", "introduce"]):
       with st.chat_message("assistant"):
         with st.spinner("Thinking..."):
