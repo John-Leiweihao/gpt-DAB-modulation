e3edda308391b5b117e905b685fce6272ef41a6c
John-Leiweihao <138362179+John-Leiweihao@users.noreply.github.com>
2024-04-08 21:14:04 +0800

    Update main.py

--- main.py
+++ main.py
@@ -168,7 +168,7 @@ if prompt := st.chat_input("Your question"):  # Prompt for user input and save t
       with st.chat_message("assistant"):
         with st.spinner("Thinking..."):
             Current_Stress,Current_Stress1,nZVS,nZCS,P,pos,plot,M=test2.PINN(st.session_state.Uin,st.session_state.Uo,st.session_state.P,st.session_state.M)
-            Answer=test2.answer(pos,st.session_state.M ,Current_Stress,Current_Stress1,nZVS,P,M)
+            Answer=test2.answer(pos,st.session_state.M ,Current_Stress,Current_Stress1,nZVS,nZCS,P,M)
             reply=Answer
             st.write(reply)
             st.image(plot)
