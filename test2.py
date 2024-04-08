6c610e4053ed7e1b49c809da5f17699c92c7d301
John-Leiweihao <138362179+John-Leiweihao@users.noreply.github.com>
2024-04-08 22:01:06 +0800

    Update test2.py

--- test2.py
+++ test2.py
@@ -159,7 +159,7 @@ def PINN(Vin, Vref, P_required, modulation):
             lower_bound = [-0.36, Optimal_D1D2[0] - 0.0001,
                        Optimal_D1D2[1] - 0.0001,
                        0, 0]
-            np.random.seed(234)
+            np.random.seed(735)
             obj, optimal_x = optimize_cs(150, model_implicit_PINN, P_required, Vin, Vref, "5DOF",upper_bound,lower_bound,bh_strategy,vh_strategy)
             ipp, P_predicted, pred, inputs, ZVS, ZCS, penalty = obj_func(optimal_x[None], model_implicit_PINN, P_required, Vin,
                                                                  Vref, with_ZVS=True, modulation="5DOF", return_all=True,threshold_ZVS=15e-2)
