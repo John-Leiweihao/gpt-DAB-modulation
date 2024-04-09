64bce3147dadc7ca834c19f34cd27b0aeb815f63
John-Leiweihao <138362179+John-Leiweihao@users.noreply.github.com>
2024-04-09 12:59:54 +0800

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,8 @@ st.info( "Hello, I am a robot designed specifically for DAB!", icon="🤟")
 with st.sidebar:
   st.markdown("<h1 style='color: #FF5733;'>PE-GPT (v2.0)</h1>", unsafe_allow_html=True)
   st.markdown('---')
-  st.markdown('\n- SPS\n- EPS\n- DPS\n- TPS\n- 5DOF')
+  #st.markdown('\n- SPS:\n- EPS\n- DPS\n- TPS\n- 5DOF')
+  st.markdown('\n-supports the design of modulation strategies for the dual active bridge converter .)
   st.markdown('---')
 clear_button=st.sidebar.button('Clear Conversation',key='clear')
 
