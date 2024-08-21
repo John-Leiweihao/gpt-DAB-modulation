962cc1aed8f5cd0cbb8b93bf4944a836fe179a98
Lei Wei Hao <138362179+John-Leiweihao@users.noreply.github.com>
2024-08-21 12:32:22 +0800

    Update main1.py

--- main1.py
+++ main1.py
@@ -233,7 +233,7 @@ if prompt := st.chat_input("Your question"):  # Prompt for user input and save t
         decision = response.response
         if decision.rfind("recommend") != -1:
             # 获取 "recommend" 之后的部分
-            recommend_index = decision.index("recommend")
+            recommend_index = decision.rfind("recommend")
             subsequent_decision = decision[recommend_index + len("recommend"):]
             first_keyword = None
             for keyword in ["SPS", "DPS", "EPS", "TPS", "5DOF"]:
